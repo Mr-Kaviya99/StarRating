e66a68152e7b98007ea392c5fe5be3abf9e6910b
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2023-04-20 21:35:42 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StarRating';
+  starRatingForm = new FormGroup({
+    starRating: new FormControl(''),
+  });
+
+  clickMe() {
+    console.log(this.starRatingForm.get('starRating')?.value!)
+  }
 }
